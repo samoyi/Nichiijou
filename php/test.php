add5f72df98fc7b4f83f1aae0d59ac52a2273546
samoyi <leening529@163.com>
2016-10-15 00:14:49 +0800

    init

---
+++ php/test.php
@@ -0,0 +1,13 @@
+﻿<meta charset="utf-8">
+<pre><?php
+
+require 'Nichiijou.class.php';
+$Nichiijou = new Nichiijou();
+
+$array1 = [1, 2, 3, 4, 5];
+$array2 = [3, 4, 5, 6, 7];
+print_r( $Nichiijou->differentItemsInTwoIndexedArrays( $array1, $array2 ) ); 
+
+
+
+?></pre>
