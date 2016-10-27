33794cbf53c5582670ade3ba07483622bb9ef99f
samoyi <leening529@163.com>
2016-10-27 23:52:19 +0800

    none

---
+++ js/Nichijou_Event.js
@@ -0,0 +1,46 @@
+
+
+function Nichijou_Event(name, age, job)
+{
+	
+	// common properties ---------------------------------------------------------------------------------------
+	
+	
+	
+    // common functions ----------------------------------------------------------------------------------------
+	
+	
+
+    
+
+    // public properties ---------------------------------------------------------------------------------------
+    
+
+
+    
+    // public methods ------------------------------------------------------------------------------------------
+    if (typeof this.roughDistance != "function")
+    {
+        // 根据两点坐标计算粗略的地表距离
+        /*
+         *  地球直径使用固定值6371公里
+         *  北纬正号，南纬符号；东经正号，西经符号。
+         */
+		/*
+		 *  TODO  参数先写经度
+		 */	
+        /* Nichijou_Geography.prototype.roughDistance = function(nLongitude1, nLatitude1, nLongitude2, nLatitude2) 
+        {
+			return sphericalDistance(EARTH_RADIUS, nLongitude1, nLatitude1, nLongitude2, nLatitude2);
+        } */
+
+    }
+}
+
+
+
+
+
+
+
+  
--- js/Nichijou_Geography.js
+++ js/Nichijou_Geography.js
@@ -1,30 +1,64 @@
 
 
-
-
-
-function degreeToRadian(nDegree) 
+function Nichijou_Geography()
 {
-    return nDegree * Math.PI / 180;
+	
+	// common properties ---------------------------------------------------------------------------------------
+	const EARTH_RADIUS = 6371;
+	
+	
+    // common functions ----------------------------------------------------------------------------------------
+	// 角度转弧度
+	function degreeToRadian(nDegree) 
+    {
+        return nDegree * Math.PI / 180;
+    }
+	
+	// 弧度转角度
+	function radianToDegree(nRadian) 
+    {
+        return nRadian * 180 / Math.PI;
+    }
+	
+	// 标准球面两点球面距离
+    function sphericalDistance(r, nLongitude1, nLatitude1, nLongitude2, nLatitude2)
+    {
+		nLatitude1 = degreeToRadian(nLatitude1);
+		nLongitude1 = degreeToRadian(nLongitude1);
+		nLatitude2 = degreeToRadian(nLatitude2);
+		nLongitude2 = degreeToRadian(nLongitude2);
+        return r * Math.acos( Math.sin(nLatitude1)*Math.sin(nLatitude2) + Math.cos(nLatitude1)*Math.cos(nLatitude2)*Math.cos(nLongitude1-nLongitude2) );
+    }
+	
+	
+
+    
+
+    // public properties ---------------------------------------------------------------------------------------
+    
+
+
+    
+    // public methods ------------------------------------------------------------------------------------------
+    if (typeof this.roughDistance != "function")
+    {
+        // 根据两点坐标计算粗略的地表距离
+        /*
+         *  地球直径使用固定值6371公里
+         *  北纬正号，南纬符号；东经正号，西经符号。
+         */
+        Nichijou_Geography.prototype.roughDistance = function(nLongitude1, nLatitude1, nLongitude2, nLatitude2) 
+        {
+			return sphericalDistance(EARTH_RADIUS, nLongitude1, nLatitude1, nLongitude2, nLatitude2);
+        }
+
+    }
 }
+
+
+
+
+
+
 
   
-// 根据两点坐标计算直线距离
-/*
- *  地球直径使用固定值6371公里
- *  北纬正号，南纬符号；东经正号，西经符号。
- */
-function calcCrow(nLatitude1, nLongitude1, nLatitude2, nLongitude2) 
-{
-    var EARTH_RADIUS = 6371;
-    var dLat = degreeToRadian(nLatitude2-nLatitude1);
-    var dLon = degreeToRadian(nLongitude2-nLongitude1);
-    var nLatitude1 = degreeToRadian(nLatitude1);
-    var nLatitude2 = degreeToRadian(nLatitude2);
-
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(nLatitude1) * Math.cos(nLatitude2); 
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-    var d = EARTH_RADIUS * c;
-    return d;
-}
