28941186f0e9216d8cb31b4e7768bbdfbd2d0d8c
FuncaIDT <info@funca.com.cn>
2016-10-28 16:52:24 +0800

    添加了B站抢一楼脚本

--- js/Nichijou_Event.js
+++ js/Nichijou_Event.js
@@ -1,4 +1,4 @@
-
+"use strict";
 
 function Nichijou_Event(name, age, job)
 {
@@ -19,20 +19,74 @@ function Nichijou_Event(name, age, job)
 
     
     // public methods ------------------------------------------------------------------------------------------
-    if (typeof this.roughDistance != "function")
+    if (typeof this.simulateEvent != "function")
     {
-        // 根据两点坐标计算粗略的地表距离
+        // 模拟事件
         /*
-         *  地球直径使用固定值6371公里
-         *  北纬正号，南纬符号；东经正号，西经符号。
+         *  
+         *  
          */
-		/*
-		 *  TODO  参数先写经度
+		/* 
+		 *  TODO  目前只支持 eventsCanSimulate 中的事件
 		 */	
-        /* Nichijou_Geography.prototype.roughDistance = function(nLongitude1, nLatitude1, nLongitude2, nLatitude2) 
-        {
-			return sphericalDistance(EARTH_RADIUS, nLongitude1, nLatitude1, nLongitude2, nLatitude2);
-        } */
+        Nichijou_Event.prototype.simulateEvent = function simulateEvent(element, eventName)
+		{
+			let eventsCanSimulate = {
+				'HTMLEvent': /^(?:load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll)$/,
+				'MouseEvent': /^(?:click|dblclick|mouse(?:down|up|over|move|out))$/
+			};
+			let defaultEventProperties = {
+				pointerX: 0,
+				pointerY: 0,
+				button: 0,
+				ctrlKey: false,
+				altKey: false,
+				shiftKey: false,
+				metaKey: false,
+				bubbles: true,
+				cancelable: true
+			};
+
+			let options = Object.assign(defaultEventProperties, arguments[2] || {});
+			let oEvent, eventType = null;
+
+			for (let name in eventsCanSimulate)
+			{
+				if (eventsCanSimulate[name].test(eventName)) 
+				{ 
+					eventType = name; 
+					break; 
+				}
+			}
+
+			if (!eventType)
+				throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported');
+
+			if (document.createEvent)
+			{	
+				oEvent = document.createEvent(eventType);
+				if (eventType == 'HTMLEvents')
+				{
+					oEvent.initEvent(eventName, options.bubbles, options.cancelable);
+				}
+				else
+				{
+					oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, document.defaultView,
+					options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
+					options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, element);
+				}
+				element.dispatchEvent(oEvent);
+			}
+			else
+			{
+				options.clientX = options.pointerX;
+				options.clientY = options.pointerY;
+				let evt = document.createEventObject();
+				oEvent = Object.assign(evt, options);
+				element.fireEvent('on' + eventName, oEvent);
+			}
+			return element;
+		}
 
     }
 }
