ae0f1a4f16514ef0809aa35ad962bddb8a6270f0
FuncaIDT <info@funca.com.cn>
2016-11-18 19:17:16 +0800

    正在整理跨域的方法

---
+++ js/Nichijou_CrossOrigin.js
@@ -0,0 +1,52 @@
+"use strict";
+
+function Nichijou_CrossOrigin()
+{
+	
+	// common properties ---------------------------------------------------------------------------------------
+	
+	
+	
+    // common functions ----------------------------------------------------------------------------------------
+	
+	
+
+    
+
+    // public properties ---------------------------------------------------------------------------------------
+    
+
+
+    
+    // public methods ------------------------------------------------------------------------------------------
+    if (typeof this.imagePing !== "function")
+    {
+        // imagePing
+        /*
+         * The argument is configuration object, contains one required property and two optional properties.
+		 * required property: 'sUrlWithArguments' , the value should be the requested url, and arguments if needed. Like 'http://www.xxx.com/index.php?name=33'
+         * optional properties: 'fnSuccess' should be a callback function when request success, and 'fnFail' should be a function triggered when request fail
+         */
+		Nichijou_CrossOrigin.prototype.imagePing = function( oConfig )
+		{
+			let img = new Image(); 
+			
+			let sUrlWithArguments = oConfig.sUrlWithArguments,
+				fnSuccess = oConfig.fnSuccess,
+				fnFail = oConfig.fnFail;
+
+			img.addEventListener('load', fnSuccess, false); 
+			img.addEventListener('error', fnFail, false); 
+			img.src = sUrlWithArguments;
+		}
+ 
+    }
+}
+
+
+
+
+
+
+
+  
---
+++ js/test.php
@@ -0,0 +1,15 @@
+﻿<pre>
+<?php   
+
+
+if( $_GET["name"] === "lining" )
+{
+	file_put_contents("right.txt", "233333333");
+}
+
+
+
+
+?>
+	
+</pre>
