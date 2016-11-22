b677bfed7061e7119f65bc2d55ca1ef18d339247
FuncaIDT <info@funca.com.cn>
2016-11-22 19:53:20 +0800

    添加历史管理类

---
+++ js/Nichijou_History.js
@@ -0,0 +1,42 @@
+"use strict";
+
+function Nichijou_History()
+{
+	
+	// common properties ---------------------------------------------------------------------------------------
+	
+	
+	
+    // common functions ----------------------------------------------------------------------------------------
+	
+	
+
+    
+
+    // public properties ---------------------------------------------------------------------------------------
+    Nichijou_History.prototype.length = 
+
+
+    
+    // public methods ------------------------------------------------------------------------------------------
+    if (typeof this.go != "function")
+    {
+        // history.go	
+        Nichijou_History.prototype.go = function(nStep)
+		{
+			window.history.go(nStep);
+		}
+		
+
+    }
+}
+
+Object.defineProperty()
+
+
+
+
+
+
+
+  
