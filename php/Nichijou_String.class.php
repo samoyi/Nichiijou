b335814d3d1b733accf891f3df627da0b5704f19
FuncaIDT <info@funca.com.cn>
2016-11-28 16:46:12 +0800

    add php string class

---
+++ php/Nichijou_String.class.php
@@ -0,0 +1,15 @@
+﻿<?php
+
+
+class Nichijou_String
+{
+
+	public function firstDiffChar( $str1, $str2 )
+	{
+		$index = strspn($str1 ^ $str2, "\0");
+		return array(
+			$index, $str1[$index], $str2[$index]
+		);
+	}
+}
+?>
--- php/test.php
+++ php/test.php
@@ -1,32 +1,15 @@
 ﻿<meta charset="utf-8">
 <pre><?php
 
-/* require 'Nichijou_XML.class.php';
-$NX = new Nichijou_XML();
+require 'Nichijou_String.class.php';
+$NS = new Nichijou_String();
 
-$array = array(
-	"appid"=> "wxd930ea5d5a258f4f",
-	"mch_id"=> 10000100,
-	"device_info"=> 1000,
-	"body"=> "test",
-	"nonce_str"=> "ibuaiVcKdpRxkhJA",
-	"sign"=>"9A0A8659F005D6984697E2CA0A9CF3B7"
-);
-$sRootTag = "xml";
+$str1 = "hello world";
+$str2 = "helle world";
 
-$result = $NX->arrayToXML($array, $sRootTag);
-print_r( $result );  */
+$result = $NS->firstDiffChar( $str1, $str2 );
+print_r($result);
 
-
-
-$test_array = array (
-  'bla' => 'blub',
-  'foo' => 'bar',
-  'stack' => 'overflow'
-);
-$xml = new SimpleXMLElement('<root/>');
-array_walk_recursive($test_array, array ($xml, 'addChild'));
-file_put_contents("xml.xml", $xml->asXML());
 
 
 
