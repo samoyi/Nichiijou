7fe66724189507dc8b1e180c547cb9242b717b9a
samoyi <greatlining@163.com>
2017-02-17 12:52:04 +0800

    压缩图片功能实现但尚未优化

--- js/CompressImage/test.php
+++ js/CompressImage/test.php
@@ -1,8 +1,13 @@
 
 <?php
-
+	
+	define('IMAGE_DIR', 'http://127.0.0.1/gits/Nichijou/js/CompressImage/images/');
+	
 	$file_base64 = $_POST['file_base64'];
 	$file_base64 = base64_decode($file_base64);
-	file_put_contents('./hehe.jpg', $file_base64);
+	$sImageName = rand().'.jpg';
+	file_put_contents('images/'.$sImageName, $file_base64); // TODO 这里固定成jpg了
+	
+	echo IMAGE_DIR . $sImageName;
 
 ?>
