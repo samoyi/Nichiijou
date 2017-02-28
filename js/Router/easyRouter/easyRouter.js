ec62b40c4c8b8b910841fc3e8a42e1db4b7266cc
samoyi <greatlining@163.com>
2017-02-28 20:03:28 +0800

    正在修改easyRouter，需要判定点击的是不是绑定的anchor

--- js/Router/easyRouter/easyRouter.js
+++ js/Router/easyRouter/easyRouter.js
@@ -1,43 +1,52 @@
 "use strict";
 
-// 第一个   
+/*
+ * 原理：
+ *   1. 给每一个锚点href绑定一个处理函数
+ *   2. 监听hash变化，如果变化为某个锚点的href，则执行其对应的处理函数
+ */ 
 
 /*
  * 第一个参数是所有的a标签节点组成的数组
  * 第二个参数是是点击某个a标签要执行的函数所组成的数组
  */
+ 
+
+
+
 function addEasyRouter(anchors, callbacks)
-{	
-	function Router(){}
-
-	Router.prototype.route = function (path, callback)
+{
+	var aHash = [];
+	Array.prototype.forEach.call(anchors, function(item)
 	{
-		var url = location.hash || '#'; 
-		
-		// 刷新时的处理
-		window.addEventListener('load', function ()
-		{
-			path = path.slice(path.lastIndexOf("#"));
-			if ( url == path ){
-				callback && callback();
-			}
-		}, false); 
-		
-		// hash变化时的处理
-		window.addEventListener('hashchange', function ()
-		{	
-			url = location.hash || '#';
-			path = path.slice(path.lastIndexOf("#"));
-			if (url == path){
-				callback && callback();
-			}
-		}, false);
-	};
-	Router = new Router();
-	
-	for(let i=0,len=anchors.length; i<len; i++)
-	{	
-		Router.route(anchors[i].href, callbacks[i]);
-	}
+		var sHref = item.href;
+		aHash.push( sHref.slice(sHref.lastIndexOf("#") ));
+	});
 
+	// 刷新时的处理
+	window.addEventListener('load', function ()
+	{
+		var nAnchorIndex = aHash.indexOf(location.hash);
+		if ( nAnchorIndex !== -1  ){
+			callbacks[nAnchorIndex]();
+		}
+	}, false); 
+	
+	// hash变化时的处理
+	window.addEventListener('hashchange', function ()
+	{	
+		var sCurrentHash = location.hash,
+			nAnchorIndex = aHash.indexOf(sCurrentHash);
+			console.log( sCurrentHash );
+			//console.log( nAnchorIndex );
+		if ( nAnchorIndex !== -1  ){
+			
+			if( "#" === sCurrentHash ){
+				location.hash = "";
+				alert();
+			}
+			callbacks[nAnchorIndex]();
+		}
+	}, false);	
+	
 }
