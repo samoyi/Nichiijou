b1ce5718177556facfac61bfc5671ff59fac59fe
samoyi <greatlining@163.com>
2017-04-10 16:46:23 +0800

    Nichijou_Math.js加入了createNonReplicativeInt函数，并且把整个文件变成了函数的简单罗列，而不是面向对象的形式。其他的JS文件也应该这样改

--- js/Nichijou_Math.js
+++ js/Nichijou_Math.js
@@ -1,60 +1,64 @@
 "use strict";
 
-function Nichijou_Math()
+
+// 数字转化为计算机进制
+/*
+* 返回一个最多五项最少一项的数组
+* 数组项从最右到最左的单位如下 ["GB", "MB", "KB", "Byte", "bit"]
+*/
+function num2ComputerRadix(num)
 {
+	if( Number.isSafeInteger(num) && num>-1 )
+	{
+		let aResult = [],
+			nBit = num%8,
+			nByte = Math.floor( num/8 ),
+			nAfterMod = nByte;
+			aResult.unshift(nBit);
 
-	// common properties ---------------------------------------------------------------------------------------
+		while( nAfterMod )
+		{
+			if( aResult.unshift( nAfterMod%1024 ) > 4 ){
+				break;
+			}
+			else{
+				nAfterMod = Math.floor( nAfterMod/1024 );
+			}
+		}
+		return aResult;
+	}
+	else
+	{
+		throw new RangeError("The number passed to function num2ComputerUnit should be a non-negative integer and smaller than 'Number.MAX_SAFE_INTEGER', which is " + Number.MAX_SAFE_INTEGER + " in decimal.");
+	}
+}
 
 
 
-    // common functions ----------------------------------------------------------------------------------------
+// 在指定闭区间内生成不重复的nNum个整数
+function createNonReplicativeInt(nMin, nMax, nNum)
+{
+	if( typeof arguments[0] !== "number" || typeof arguments[0] !== "number" || typeof arguments[0] !== "number" )
+	{
+		throw new TypeError( "Function createNonReplicativeInt has a wrong type argument");
+	}
+	if( arguments[0]>arguments[1] || arguments[2]>(arguments[1]-arguments[0]+1) )
+	{
+		throw new RangeError( "One of arguments of Function createNonReplicativeInt is out of range");
+	}
 
+	let aResult = [];
 
+	let nCreated = 0;
+	while( nCreated < nNum )
+	{
+		let nRan = Math.floor( Math.random()*(nMax-nMin+1)+nMin );
+		if( aResult.indexOf(nRan)<0 )
+		{
+			aResult.push(nRan);
+			nCreated++;
+		}
+	}
 
-
-
-    // public properties ---------------------------------------------------------------------------------------
-
-
-
-
-    // public methods ------------------------------------------------------------------------------------------
-		if (typeof this.num2ComputerRadix != "function")
-    {
-        // 数字转化为计算机进制
-				/*
-				 * 返回一个最多五项最少一项的数组
-				 * 数组项从最右到最左的单位如下 ["GB", "MB", "KB", "Byte", "bit"]
-				 */
-        Nichijou_Math.prototype.num2ComputerRadix = function (num)
-				{
-					if( Number.isSafeInteger(num) && num>-1 )
-					{
-
-						let aValue = [],
-							nBit = num%8,
-							nByte = Math.floor( num/8 ),
-							nAfterMod = nByte;
-						aValue.unshift(nBit);
-
-						while( nAfterMod )
-						{
-							if( aValue.unshift( nAfterMod%1024 ) > 4 )
-							{
-								break;
-							}
-							else
-							{
-								nAfterMod = Math.floor( nAfterMod/1024 );
-							}
-						}
-						return aValue;
-					}
-					else
-					{
-						throw new RangeError("The number passed to function num2ComputerUnit should be a non-negative integer and smaller than 'Number.MAX_SAFE_INTEGER', which is " + Number.MAX_SAFE_INTEGER + " in decimal.");
-					}
-				}
-
-		    }
+	return aResult;
 }
