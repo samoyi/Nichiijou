c5f3a69be1296a650b75ee6dbefca8e17eb978be
samoyi <greatlining@163.com>
2017-04-11 20:22:08 +0800

    修改removeArrayItem

--- js/Nichijou_Array.js
+++ js/Nichijou_Array.js
@@ -2,11 +2,18 @@
 
 
 // 从 aArray 数组中删除出现在 aItem 中的每一项
+/*
+ *  直接修改原数组
+ *  返回值是 aArray 中实际删除了 aItem 中的哪些项
+ */
 function removeArrayItem(aArray, aItem)
 {
+
     if( !Array.isArray(aArray) || !Array.isArray(aItem) ){
         throw TypeError( "Argument type of removeArrayItem must be Array" );
     }
+
+    let aRemoved = [];
 
     let aItemNum = aItem.length,
         nThisTelIndex = -1;
@@ -14,11 +21,15 @@ function removeArrayItem(aArray, aItem)
     for(let i=0; i<aItemNum; i++)
     {
         nThisTelIndex = aArray.indexOf(aItem[i]);
-        while( nThisTelIndex>-1  )
+        if( nThisTelIndex>-1  )
         {
-            aArray.splice(nThisTelIndex, 1);
-            nThisTelIndex = aArray.indexOf(aItem[i]);
+            aRemoved.push(aItem[i]);
+            do{
+                aArray.splice(nThisTelIndex, 1);
+                nThisTelIndex = aArray.indexOf(aItem[i]);
+            }
+            while( nThisTelIndex>-1  )
         }
     }
-    return aArray;
+    return aRemoved;
 }
