7f29f9c5b7125e6baf686c5c83cd6c662a861bd6
samoyi <greatlining@163.com>
2017-05-11 20:13:42 +0800

    正在写SSE

---
+++ communication/cross-origin/Comet/SSE/test.php
@@ -0,0 +1,29 @@
+<?php
+	
+	header("Content-Type: text/event-stream"); 
+	
+
+		/* echo 'data: normal data ' ;
+		echo "\n\n";
+		echo 'id: 1';
+		echo '\n\n'; */
+
+	/* while (1) 
+	{
+		echo "event: ping\n";
+		$curDate = date(DATE_ISO8601);
+		echo 'data: {"time": "' . $curDate . '"}';
+		echo "\n\n";
+
+		echo 'data: normal data ' . "\n\n";
+	  
+		ob_end_flush();
+		flush();
+		sleep(3);
+	} */
+	
+
+
+
+?>
+ 
