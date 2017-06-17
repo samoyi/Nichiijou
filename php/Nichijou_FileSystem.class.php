96d181d91786c0470033ff91e7766999cfb9bf19
samoyi <greatlining@163.com>
2017-06-17 17:37:05 +0800

    Nichijou_FileSystem.class.php增加了按自然数重命名多个文件的方法

--- php/Nichijou_FileSystem.class.php
+++ php/Nichijou_FileSystem.class.php
@@ -5,7 +5,7 @@ class Nichijou_FileSystem
     // 整体复制并移动
     /*
      * 将src目录下的文件及文件夹复制到dest目录下
-     * aIgnore参数中每一项为不复制的文件名或文件夹名
+     * $aIgnore数组中每一项为不复制的文件名或文件夹名
      * 不能出现中文
      */ 
     public function copyRecurse($src, $dest, $aIgnore=array()) 
@@ -27,7 +27,35 @@ class Nichijou_FileSystem
             } 
         } 
         closedir( $dir ); 
-    }     
+    }   
+
+		
+	// 将文件按照从0开始的整数序号重命名	
+	/*
+	 * @ para $src      对该路径下的文件进行重命名
+	 * @ para $aIgnore  该数组中的文件将被忽略，不进行重命名
+	 * @ return         原来名字组成的数组，数组index是现在的名字
+	 */
+	public function indexFiles($src, $aIgnore=array())
+	{
+		$dir = opendir($src); 
+        @mkdir($dest); 
+		
+		$oldName = array();
+		$index = 0;
+		
+        while(false !== ( $file = readdir($dir)) ) 
+        { 	
+            if( ( $file != '.' ) && ( $file != '..' ) && is_file($src.'/'.$file) && !in_array($file, $aIgnore) ) 
+            { 
+				$extension = pathinfo($file, PATHINFO_EXTENSION);
+				$oldName[] = $file;
+                rename($src.'/'.$file, $src.'/'.$index++.'.'.$extension);
+            } 
+        } 
+        closedir( $dir ); 
+		return $oldName;
+	}
 }
 
 
