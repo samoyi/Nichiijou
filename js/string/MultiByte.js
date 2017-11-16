d4b13393cc35d19111aa65b85ace913cad2c2a5f
samoyi <greatlining@163.com>
2017-11-16 19:09:25 +0800

    debug MultiByte.js

--- js/string/MultiByte.js
+++ js/string/MultiByte.js
@@ -1,10 +1,5 @@
-
-function MB(str){
+function MB(){
     'use strict';
-
-    this.str = str;
-    this.arr = [...this.str];
-    // let arr = [...str];
 
     const check = function(str){
         let arr = [...str];
@@ -22,21 +17,23 @@ function MB(str){
         return aMBCharIndex;
     };
 
-    const pad = (direction, targetLength, padString=" ")=>{
-        let fnOrigin = direction==='end' ? ''.padEnd.bind(this.str) : ''.padStart.bind(this.str);
+    const pad = (str, direction, targetLength, padString=" ")=>{
+        let fnOrigin = direction==='end' ? ''.padEnd.bind(str) : ''.padStart.bind(str);
 
         if(padString===" ") return fnOrigin(targetLength, padString);
 
-        let aMBCharIndex = check(padString);
-        if(aMBCharIndex.length){
+        let aMBCharIndexInStr = check(str),
+            aMBCharIndexInPadStr = check(padString);
+
+        if(aMBCharIndexInStr.length || aMBCharIndexInPadStr.length){
             let aPadStr = [...padString],
                 nPadStrLen = aPadStr.length,
-                nPadLen = targetLength - this.arr.length,
+                nPadLen = targetLength - [...str].length,
                 nRepeatTimes = Math.floor(nPadLen/nPadStrLen),
                 mod = nPadLen%nPadStrLen,
                 sPad = padString.repeat(nRepeatTimes) + aPadStr.slice(0, mod).join('');
 
-            return direction==='end' ? this.str + sPad : sPad + this.str;
+            return direction==='end' ? str + sPad : sPad + str;
         }
         else{
             return fnOrigin(targetLength, padString);
@@ -46,74 +43,80 @@ function MB(str){
 
 
 
-    this.check = ()=>check(this.str);
+    this.check = str=>check(str);
 
 
-    this.length = this.arr.length;
+    this.length = str=>[...str].length;
 
 
-    this.charAt = (index)=>this.arr[index];
+    this.charAt = (str, index)=>[...str][index];
 
 
-    this.charCodeAt = (index)=>this.str.codePointAt(index);
+    this.charCodeAt = (str, index)=>[...str][index].codePointAt(0);
 
 
-    this.fromCharCode = (nCode)=>String.fromCodePoint(nCode);
+    this.fromCharCode = nCode=>String.fromCodePoint(nCode);
 
 
-    this.indexOf = (substr)=>this.arr.indexOf(substr);
+    this.indexOf = (str, substr)=>{
+        let index = str.indexOf(substr),
+            num = check(str.slice(0, index)).length;
+        return index - num;
+    };
 
 
-    this.lastIndexOf = (substr)=>this.arr.lastIndexOf(substr);
+    this.lastIndexOf = (str, substr)=>{
+        let index = str.lastIndexOf(substr),
+            num = check(str.slice(0, index)).length;
+        return index - num;
+    };
 
 
-    this.match = (re)=>{
-        let result = this.str.match(re);
+    this.match = (str, re)=>{
+        let result = str.match(re);
 
         if(result && result[0]!=='' && result.index!==undefined){
-            let substr = this.str.slice(0, result.index),
+            let substr = str.slice(0, result.index),
                 aMBCharIndex = check(substr);
             result.index -= aMBCharIndex.length;
         }
-
         return result;
     };
 
 
-    this.padEnd = (targetLength, padString=" ")=>{
-        return pad('end', targetLength, padString);
+    this.padEnd = (str, targetLength, padString=" ")=>{
+        return pad(str, 'end', targetLength, padString);
     };
 
 
-    this.padStart = (targetLength, padString=" ")=>{
-        return pad('start', targetLength, padString);
+    this.padStart = (str, targetLength, padString=" ")=>{
+        return pad(str, 'start', targetLength, padString);
     };
 
 
-    this.search = (re)=>{
-        let index = this.str.search(re);
+    this.search = (str, re)=>{
+        let index = str.search(re);
         if(index>1){
-            index -= check( this.str.slice(0, index) ).length;
+            index -= check(str.slice(0, index) ).length;
         }
         return index;
     };
 
 
-    this.slice = (beginIndex, endIndex=undefined)=>this.arr.slice(beginIndex, endIndex).join('');
+    this.slice = (str, beginIndex, endIndex=undefined)=>[...str].slice(beginIndex, endIndex).join('');
 
 
-    this.split = ()=>this.arr;
+    this.split = str=>[...str];
 
 
-    this.substr = (start, length=undefined)=>{
-        if( check(this.str))
+    this.substr = (str, start, length=undefined)=>{
         if(length<1) return '';
         let len = length ? start+length : undefined;
-        return this.arr.slice(start, len).join('');
+        return [...str].slice(start, len).join('');
     };
 
 
-    this.substring = (start, end)=>{
+    this.substring = (str, start, end)=>{
         if(!start || start<0){ // NaN, null or negative
             start = 0;
         }
@@ -121,11 +124,8 @@ function MB(str){
             end = 0;
         }
         if(start>end){
-            return this.arr.slice(end, start).join('');
+            return [...str].slice(end, start).join('');
         }
-        return this.arr.slice(start, end).join('');
+        return [...str].slice(start, end).join('');
     };
-
-
-    this.reverse = ()=>this.arr.reverse().join('');
 }
