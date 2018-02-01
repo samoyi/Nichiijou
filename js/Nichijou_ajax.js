76df321449d05e74cd3a8d6a074b926337bb49a9
samoyi <greatlining@163.com>
2018-02-01 19:59:15 +0800

    完成了简单的fetch get和post封装

--- js/Nichijou_ajax.js
+++ js/Nichijou_ajax.js
@@ -17,7 +17,7 @@ const XHR = {
 	* @param fnSuccessCallback  请求成功之后的回调函数。接受一个参数用来获取xhr.responseText
 	* @param fnFailCallback     请求失败之后的回调函数。接受一个参数用来获取xhr.status
 	*/
-	get(sURL, fnSuccessCallback, fnFailCallback){
+	xhr_get(sURL, fnSuccessCallback, fnFailCallback){
 		let xhr = new XMLHttpRequest();
 		xhr.addEventListener('readystatechange', function()
 		{
@@ -43,7 +43,7 @@ const XHR = {
 	* @param fnSuccessCallback  请求成功之后的回调函数。接受一个参数用来获取xhr.response
 	* @param fnFailCallback     请求失败之后的回调函数。接受一个参数用来获取xhr.status
 	*/
-	getBlob(sURL, fnSuccessCallback, fnFailCallback){
+	xhr_getBlob(sURL, fnSuccessCallback, fnFailCallback){
 		let xhr = new XMLHttpRequest();
 		xhr.addEventListener('readystatechange', function(){
 			if (xhr.readyState == 4)
@@ -63,13 +63,13 @@ const XHR = {
 
 
 	// POST
-	/*
+	/**
 	* @param sURL               请求URL
 	* @param data               request body
 	* @param fnSuccessCallback  请求成功之后的回调函数。接受一个参数用来获取xhr.responseText
 	* @param fnFailCallback     请求失败之后的回调函数。接受一个参数用来获取xhr.status
 	*/
-	post(sURL, data, fnSuccessCallback, fnFailCallback){
+	xhr_post(sURL, data, fnSuccessCallback, fnFailCallback){
 		let xhr = new XMLHttpRequest();
 		xhr.addEventListener('readystatechange', function()
 		{
@@ -93,45 +93,75 @@ const XHR = {
 
 
 const FETCH = {
-	async get(sURL, fnSuccessCallback, fnFailCallback){
-		try{
-			let res = await fetch(sURL);
-			fnSuccessCallback(res);
-		}
-		catch(err){
-			fnFailCallback(err);
-		}
+	// fetch get
+	/**
+	* @param sURL               请求URL
+	* @param sReadType          读取response的方法名字符串：
+	*								arrayBuffer、blob、formData、json和text
+	* @param oInit				fetch()方法的第二个参数
+	* @return            		使用sReadType指定的方法读取response返回的
+	*                           	promise对象。solve之后的res参数为一个对象
+	*                               ，如果请求出错res.err为错误码，如果没出错，
+	*                               res.result为上述promise读取成功的数据
+	*/
+	async fetch_get(sURL, sReadType, oInit){
+	    try{
+	        let response = await fetch(new Request(sURL, oInit));
+	        if(!response.ok && response.status!==304){
+	            throw new Error(response.status)
+	        }
+
+	        let clone = response.clone();
+
+	        switch(sReadType){
+	            case 'arrayBuffer':{
+	                return await {result: await clone.arrayBuffer()};
+	            }
+	            case 'blob':{
+	                return await {result: await clone.blob()};
+	            }
+	            case 'formData':{
+	                return await {result: await clone.formData()};
+	            }
+	            case 'json':{
+	                return await {result: await clone.json()};
+	            }
+	            default:{
+	                return await {result: await clone.text()};
+	            }
+	        }
+	    }
+	    catch(err){
+	        return await {
+	            err: err.message,
+	        };
+	    }
 	},
+
+	// fetch get
+	/**
+	* @param sURL               同 fetch_get
+	* @param oBody              键值对对象
+	* @param sReadType          同 fetch_get
+	* @param oInit				同 fetch_get
+	* @return            		同 fetch_get
+	*/
+	function fetch_post(sURL, oBody, sReadType, oInit){
+	    let headers = {
+	        'Content-Type': 'application/x-www-form-urlencoded',
+	    };
+
+	    let oPostInit = {
+	        method: 'POST',
+	        headers,
+	        body: stringify(body)};
+	    return fetch_get(sURL, sReadType
+	                , Object.assign(oPostInit, oInit||{}));
+	}
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 module.exports = {
 	XHR,
-	fetch
+	FETCH,
 };
