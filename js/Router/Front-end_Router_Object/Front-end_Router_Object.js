9102ea870c0addd4a6dd5c252de1830e7f5f78b2
samoyi <leening529@163.com>
2018-06-17 22:22:28 +0800

    修改前端路由demo

--- js/Router/Front-end_Router_Object/Front-end_Router_Object.js
+++ js/Router/Front-end_Router_Object/Front-end_Router_Object.js
@@ -1,120 +1,138 @@
 /*
- * 修改自 http://krasimirtsonev.com/blog/article/A-modern-JavaScript-router-in-100-lines-history-api-pushState-hash-url	
+ * 修改自 http://krasimirtsonev.com/blog/article/A-modern-JavaScript-router-in-100-lines-history-api-pushState-hash-url
+ * 兼容 history 模式和 hash 模式。完整的 history 模式需要后端配合。
  */
 
-function createRouter()
-{
-	return {
+function createRouter(){
+
+	let config = {
 		routes: [],
 		mode: null,
 		root: '/',
-		config: function (options)
-		{
-			this.mode = options && options.mode && options.mode === 'history'
-			&& !!(history.pushState) ? 'history' : 'hash';
-			this.root = options && options.root ? '/' + this.clearEdgeSlashes(options.root) + '/' : '/';
-			return this;
-		},
-		getFragment: function ()
-		{
-			var fragment = '';
-			if (this.mode === 'history')
-			{
-				/*
-				fragment = this.clearEdgeSlashes(decodeURI(location.pathname + location.search));
-				fragment = fragment.replace(/\?(.*)$/, '');
-				*/
-				// modify1 将原作者上面两行替换为下面的一行。不知道有什么区别。
-				fragment = this.clearEdgeSlashes(decodeURI(location.pathname));
-				fragment = this.root != '/' ? fragment.replace(this.root, '') : fragment;
+	};
+
+	/*
+	 * 根据路由路径执行对应的函数
+	 * @param  {String}    path    路由路径
+	 */
+	function route(path){
+		path = path || getRoutePath() || '/';
+		config.routes.some((route)=>{
+			if ((new RegExp(path) + '') === (route.re + '')){
+				route.handler.call();
+				return true;
 			}
-			else
-			{
-				var match = window.location.href.match(/#(.*)$/);
-				fragment = match ? match[1] : '';
-			}
-			return this.clearEdgeSlashes(fragment);
-		},
-		clearEdgeSlashes: function (path)
-		{
-			return path.toString().replace(/\/$/, '').replace(/^\//, '');
-		},
-		add: function (re, handler)
-		{
-			if (typeof re == 'function')
-			{
-				handler = re;
-				re = '';
-			}
-			this.routes.push({re: re, handler: handler});
-			return this;
-		},
-		remove: function (param)
-		{	
-			for (var i = 0, r; i < this.routes.length, r = this.routes[i]; i++)
-			{
-				if (r.handler === param || r.re.toString() === param.toString())
-				{
-					this.routes.splice(i, 1);
-					return this;
-				}
-			}
-			return this;
-		},
-		flush: function ()
-		{
-			this.routes = [];
-			this.mode = null;
-			this.root = '/';
-			return this;
-		},
-		check: function (f)
-		{
-			var fragment = f || this.getFragment();
-			for (var i = 0; i < this.routes.length; i++)
-			{
-				var match = fragment.match(this.routes[i].re);
-				
-				if (match)
-				{	
-					match.shift();
-					this.routes[i].handler.apply({}, match);
-					return this;
-				}
-			}
-			return this;
-		},
-		listen: function ()
-		{
-			var self = this;
-			var current = self.getFragment();
-			var fn = function ()
-			{
-				if (current !== self.getFragment())
-				{
-					current = self.getFragment();
-					self.check(current);
-				}
-			}
-			clearInterval(this.interval);
-			this.interval = setInterval(fn, 50);
-			return this;
-		},
-		navigate: function (path)
-		{
-			path = path ? path : '';
-			if (this.mode === 'history')
-			{
-				history.pushState(null, null, this.root + this.clearEdgeSlashes(path));
-			}
-			else
-			{
-				window.location.href = window.location.href.replace(/#(.*)$/, '') + '#' + path;
-			}
-			return this;
+		});
+	}
+
+	function clearEdgeSlashes(path){
+		return path.replace(/\/$/, '').replace(/^\//, '');
+	}
+
+
+	/*
+	 * 根据 url 获取路由路径
+	 */
+	function getRoutePath(){
+		let path = '';
+		if (config.mode === 'history') {
+			path = decodeURI(location.pathname);
+			path = config.root !== '/' ? path.replace(config.root, '') : path;
 		}
+		else {
+			let match = location.href.match(/#(.*)$/);
+			path = match ? match[1] : '';
+		}
+		return clearEdgeSlashes(path);
+	}
+
+
+	// 返回单例
+	return {
+
+		/*
+		 * 配置路由模式和根目录
+		 * @param  {object}    options     配置选项
+		 */
+		config(options){
+			config.mode = options
+				&& options.mode
+				&& options.mode === 'history'
+				&& history.pushState
+					? 'history'
+					: 'hash';
+			config.root = options
+				&& options.root
+					? '/' + clearEdgeSlashes(options.root) + '/'
+					: '/';
+			return this;
+		},
+
+		/*
+		 * Add a route
+		 * @param  {RegExp}    re          用来匹配路由路径的正则表达式
+		 * @param  {Function}  handler	   路由处理函数（切换路径后要执行的操作）
+		 */
+		add(re, handler){
+			config.routes.push({re, handler});
+			return this;
+		},
+
+		/*
+		 * Remove a route
+		 * @param  {RegExp}    re     用来匹配路由路径的正则表达式
+		 */
+		remove(re){
+			config.routes.some((route, index)=>{
+				if (route.re.toString() === re.toString()){
+					return config.routes.splice(index, 1);
+				}
+			});
+			return this;
+		},
+
+		/*
+		 * Reinitialize
+		 */
+		flush(){
+			config = {
+				routes: [],
+				mode: null,
+				root: '/',
+			};
+			return this;
+		},
+
+		/*
+		 * 监听 url 变化，根据变化进行路由
+		 */
+		listen(){
+			// 页面刚加载时，根据路径进行初始路由
+			// history 模式需要后端配合
+			route(getRoutePath());
+
+			let current = getRoutePath();
+			let interval = null;
+			clearInterval(interval);
+			interval = setInterval(function (){
+				// url 发生变化，路由路径和之前的不一样
+				if (current !== getRoutePath()){
+					current = getRoutePath();
+					route(current);
+				}
+			}, 50);
+			return this;
+		},
+
+		navigate(path){
+			path = path ? path : '';
+			if (config.mode === 'history'){
+				history.pushState(null, null, config.root + clearEdgeSlashes(path));
+			}
+			else {
+				location.href = location.href.replace(/#(.*)$/, '') + '#' + path;
+			}
+			return this;
+		},
 	};
 }
-
-
-
--- js/Router/HashRouter/HashRouter.js
+++ js/Router/HashRouter/HashRouter.js
@@ -2,70 +2,52 @@
 
 /*
  * 原理：
- *   1. 给每一个锚点href绑定一个处理函数
- *   2. 监听hash变化，如果变化为某个锚点的href，则执行其对应的处理函数
- */ 
+ *   1. 给每一个锚点 href 绑定一个处理函数
+ *   2. 监听 hash 变化，如果变化为某个锚点的 href，则执行其对应的处理函数
+ */
 
 /*
- * HTML a标签href属性的写法：
- *   1. 首页链接只写#号
- *   2. 其他页面链接写#号加页面名称
- *  
+ * HTML a 标签 href 属性的写法：
+ *   1. 首页链接只写 #
+ *   2. 其他页面链接写 # 加页面名称
+ *
  *   参考 example.html
- */ 
- 
+ */
+
 /*
  * aHash参数：
- * 	 1. 所有a标签href属性值组成的数组（不包括重复的）
- *	 2. 数组首项必须是首页的href属性，即 # 
- *   
+ * 	 1. 所有 a 标签 href 属性值组成的数组（不包括重复的）
+ *	 2. 数组首项必须是首页的 href 属性，即 #
+ *
  * aCallbacks参数：
- *   数组项与aHash参数一一对应，点击aHash中某一项href所在的a标签时，则执行aCallbacks对应项的函数（更新页面内容）
+ *   数组项与 aHash 参数一一对应，点击 aHash 中某一项 href 所在的 a 标签时，则执行
+ *   aCallbacks 对应项的函数（更新页面内容）
  *
  * 参考 example.html
  */
- 
-/*
- * XXX
- *   1. hash变化为首页时，location.href会在末尾带上一个无意义的 # 号
- */
- 
 
 
+function addHashRouter(aHash, aCallbacks){
 
-function addHashRouter(aHash, aCallbacks)
-{
+    function route(){
+        let sCurrentHash = location.hash,
+			nAnchorIndex = aHash.indexOf(sCurrentHash);
+
+		if(!sCurrentHash){ // 首页，hash为空
+			aCallbacks[0]();
+		}
+		else if (nAnchorIndex !== -1){
+			aCallbacks[nAnchorIndex]();
+		}
+    }
 
 	// 新进入或刷新时的处理
-	window.addEventListener('load', function ()
-	{
-		let sCurrentHash = location.hash,
-			nAnchorIndex = aHash.indexOf(sCurrentHash);
-			
-		if( !sCurrentHash) // 首页，hash为空
-		{
-			aCallbacks[0]();
-		}
-		else if ( nAnchorIndex !== -1  )
-		{
-			aCallbacks[nAnchorIndex]();
-		}
-	}, false); 
-	
+	window.addEventListener('load', function (){
+        route();
+	});
+
 	// hash变化时的处理
-	window.addEventListener('hashchange', function ()
-	{	
-		let sCurrentHash = location.hash,
-			nAnchorIndex = aHash.indexOf(sCurrentHash);
-		
-		if( !sCurrentHash ) // 首页，hash为空
-		{
-			aCallbacks[0]();
-		}		
-		else if ( nAnchorIndex !== -1  )
-		{
-			aCallbacks[nAnchorIndex]();
-		}
-	}, false);	
-	
+	window.addEventListener('hashchange', function (){
+        route();
+	});
 }
